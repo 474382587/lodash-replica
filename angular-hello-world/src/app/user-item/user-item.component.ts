3d50f30e0107ebedb563ae8cabdbc2ccd5ada477
Joseph Jin <jerkjoe61@gmail.com>
2019-03-25 16:52:02 -0700

    .

--- angular-hello-world/src/app/user-item/user-item.component.ts
+++ angular-hello-world/src/app/user-item/user-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from "@angular/core";
 export class UserItemComponent implements OnInit {
   @Input() name: string;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
