b1ad863e37c4211741801bc5b18679a3786c1cd7
Joseph Jin <jerkjoe61@gmail.com>
2019-03-27 16:09:08 -0700

    angular

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-
-  addArticle(title:HTMLInputElement, link:HTMLInputElement): boolean {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    // ...
+  }
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title ${title.value} and link ${link.value}`);
     return false;
   }
+
 }
