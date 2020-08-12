b89f652bc5abb694776f47bbfd881ba314210673
Joseph Jin <jerkjoe61@gmail.com>
2020-08-11 17:26:27 -0700

    changes

---
+++ src/pages/fortune/details/resp.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+import Layout from "../../../components/layout"
+import { Carousel as AndtCarousel } from "antd"
+import { Row, Col, Container, Modal, Card, Button } from "react-bootstrap"
+
+// import "./services.scss"
+
+const taxAvoidance = () => {
+  
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
+
+export default taxAvoidance
