6d5e15b7dc8d8ee98f75913cec04529971a12cc1
Joseph Jin <jerkjoe61@gmail.com>
2020-08-18 23:26:04 -0700

    add links

--- src/pages/loan/details/new-home.js
+++ src/pages/loan/details/new-home.js
@@ -4,8 +4,10 @@ import { Carousel as AndtCarousel } from "antd"
 import { Row, Col, Container, Modal, Card, Button } from "react-bootstrap"
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
 // import "./services.scss"
+import { Link } from "gatsby"
+
 const Contentful = require("contentful")
-const NewHome = () => {
+const Service = () => {
   const [post, setPost] = useState({})
   const id = "6GLhHwqikqHcJuC7CrFCib"
   useEffect(() => {
@@ -32,9 +34,22 @@ const NewHome = () => {
       <Container className="service-container">
         <h1>住房按揭</h1>
         <div className="html-content"></div>
+        <Row className="nav-links">
+          <Col md="6" sm="6" xs="6" style={{ fontSize: 32 }}>
+            <Link to="/loan/services">◀ 住房贷款 </Link>
+          </Col>
+          <Col
+            md="6"
+            sm="6"
+            xs="6"
+            style={{ textAlign: "right", fontSize: 32 }}
+          >
+            <a href={post.url ? post.url : "#"}>博客 ▶</a>
+          </Col>
+        </Row>
       </Container>
     </Layout>
   )
 }
 
-export default NewHome
+export default Service
--- src/pages/loan/details/refine.js
+++ src/pages/loan/details/refine.js
@@ -4,6 +4,7 @@ import { Carousel as AndtCarousel } from "antd"
 import { Row, Col, Container, Modal, Card, Button } from "react-bootstrap"
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
 // import "./services.scss"
+import { Link } from "gatsby"
 const Contentful = require("contentful")
 const Service = () => {
   const [post, setPost] = useState({})
@@ -30,6 +31,19 @@ const Service = () => {
       <Container className="service-container">
         <h1>贷款重组</h1>
         <div className="html-content"></div>
+         <Row className="nav-links">
+          <Col md="6" sm="6" xs="6" style={{ fontSize: 32 }}>
+            <Link to="/loan/services">◀ 住房贷款 </Link>
+          </Col>
+          <Col
+            md="6"
+            sm="6"
+            xs="6"
+            style={{ textAlign: "right", fontSize: 32 }}
+          >
+            <a href={post.url ? post.url : "#"}>博客 ▶</a>
+          </Col>
+        </Row>
       </Container>
     </Layout>
   )
